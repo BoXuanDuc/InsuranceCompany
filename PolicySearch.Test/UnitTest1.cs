820623ecf3319d000b555c7bfce8622be054225a
Bồ Xuân Đức <2324802010204@student.tdmu.edu.vn>
2026-03-03 15:18:27 +0700

    Add project files.

---
+++ PolicySearch.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PolicySearch.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Pricing.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Pricing.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
