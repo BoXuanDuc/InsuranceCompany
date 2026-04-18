ec57081ad452a514dfd261f733a13ec76f8cbb3b
Duc <2324802010204@student.tdmu.edu.vn>
2026-04-18 10:15:46 +0700

    luu

--- ChatService/Startup.cs
+++ ChatService/Startup.cs
@@ -12,8 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using PolicyService.Api.Events;
 using Steeltoe.Discovery.Client;
+using PolicyService.Api.Events;
 
 namespace ChatService;
 
@@ -26,7 +26,6 @@ public class Startup
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
         var appSettingsSection = Configuration.GetSection("AppSettings");
@@ -49,35 +48,39 @@ public class Startup
 
         var appSettings = appSettingsSection.Get<AppSettings>();
         var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-        services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-            {
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateActor = false
-                };
-                x.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var accessToken = context.Request.Query["access_token"];
 
-                        var path = context.HttpContext.Request.Path;
-                        if (!string.IsNullOrEmpty(accessToken) &&
-                            path.StartsWithSegments("/agentsChat"))
-                            context.Token = accessToken;
-                        return Task.CompletedTask;
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateActor = false
+            };
+            x.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+                    var path = context.HttpContext.Request.Path;
+
+                    if (!string.IsNullOrEmpty(accessToken) &&
+                        path.StartsWithSegments("/agentsChat"))
+                    {
+                        context.Token = accessToken;
                     }
-                };
-            });
+
+                    return Task.CompletedTask;
+                }
+            };
+        });
 
         services.AddAuthorization(_ => { });
 
@@ -86,31 +89,33 @@ public class Startup
         services.AddSingleton<IUserIdProvider, NameUserIdProvider>();
 
         services.AddRabbitListeners();
-        
-        services.AddSwaggerGen();
+
         services.AddDiscoveryClient(Configuration);
+
+        services.AddSwaggerGen();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         app.UseRouting();
+        app.UseDiscoveryClient();
+
         if (env.IsDevelopment())
             app.UseDeveloperExceptionPage();
         else
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
-        
+
         if (env.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-        app.UseDiscoveryClient();
+
         app.UseCors("CorsPolicy");
         app.UseAuthentication();
         app.UseAuthorization();
         app.UseHttpsRedirection();
+
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
